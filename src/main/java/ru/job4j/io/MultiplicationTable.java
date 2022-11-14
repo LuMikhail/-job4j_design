8d84fe439145d8ccef0780d69e2d5713f35efcb3
Home <1984milu@mail.ru>
2022-11-14 15:13:00 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("multiplication_table.txt")) {
+            int[] pif = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            for (int q : pif) {
+                out.write(Integer.toString(q).getBytes());
+                out.write(" |".getBytes());
+                for (int w : pif) {
+                    out.write(Integer.toString(q * w).getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
