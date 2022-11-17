feb07f2969d4a584466b1b118c55b37200c4e7fc
Home <1984milu@mail.ru>
2022-11-18 01:06:13 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,22 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * Класс выводит на консоль только имя директории и его размер.
+ */
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subfile : file.listFiles()) {
+            System.out.printf("File name %s,", subfile.getName());
+            System.out.printf(" its size size %s%n", subfile.length());
+        }
+    }
+}
