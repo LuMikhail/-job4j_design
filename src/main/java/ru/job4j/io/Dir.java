a3ba580bd5e350984631c8da6ea5ab9f19a66ffe
Home <1984milu@mail.ru>
2022-11-29 23:09:03 +0300

    Примеры которые приведены в курсе

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,12 +4,16 @@ import java.io.File;
 
 /**
  * Класс выводит на консоль только имя файла и его размер.
- *  Данный метод работает корректно только с файлами.
- *  Для каталогов он возвращает 0, либо размер метаданных каталога, но не суммарный размер файлов в нем.
+ * Данный метод работает корректно только с файлами.
+ * Для каталогов он возвращает 0, либо размер метаданных каталога, но не суммарный размер файлов в нем.
+ * Аргумент указан в Dir - Edit configuration.
  */
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
