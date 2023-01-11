a9a347dbc6d7f57294b805a7491cae0c0d816366
Home <1984milu@mail.ru>
2023-01-11 15:02:51 +0300

    4. Slf4j - вывод exception. [#268853 #379943]

--- src/main/java/ru/job4j/io/socket/EchoServer.java
+++ src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -21,7 +21,7 @@ import ru.job4j.io.log.UsageLog4j;
  */
 public class EchoServer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
