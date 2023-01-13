ff05f5d611c1e17888a8dcef6941bfd6acaacd33
Home <1984milu@mail.ru>
2023-01-13 22:37:05 +0300

    0.1. Связи между таблицами [#373615]

---
+++ sql/fine/variants.sql
@@ -0,0 +1,42 @@
+/* many-to-many */
+create table car(
+     id serial primary key,
+     title varchar(15)
+ );
+
+ create table human(
+     id serial primary key,
+     name varchar(20)
+ );
+
+ create table driver(
+     id serial primary key,
+     car_id int references car(id),
+     human_id int references human(id)
+ );
+
+ /* one-to-one */
+ create table client(
+    id serial primary key,
+    name varchar(20),
+    number int
+);
+
+create table phone(
+    id serial primary key,
+    number int,
+    client_id int references client(id) unique
+);
+
+/* many-to-one */
+create table pages(
+    id serial primary key,
+    content varchar(250),
+    name varchar(50)
+);
+
+create table menus(
+    id serial primary key,
+    name varchar(50),
+	pages_id int references pages(id)
+);
