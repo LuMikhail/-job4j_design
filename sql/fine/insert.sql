b63f43cafa17cdb9a7f77fed2ea61254d0634c9b
Home <1984milu@mail.ru>
2023-01-16 10:37:54 +0300

    1. Схема прав пользователи и роли. [#1093 #381349]

--- sql/fine/insert.sql
+++ sql/fine/insert.sql
@@ -13,8 +13,8 @@ insert into category(category_name) values ('second');
 insert into state(status) values ('sent');
 insert into state(status) values ('received');
 
-insert into item(item_name, users_id, caregory_id, state_id) values ('reward', 1, 1, 1);
-insert into item(item_name, users_id, caregory_id, state_id) values ('vacation pay', 2, 1, 2);
+insert into item(item_name, users_id, category_id, state_id) values ('reward', 1, 1, 1);
+insert into item(item_name, users_id, category_id, state_id) values ('vacation pay', 2, 1, 2);
 
 insert into comments(comments_name, item_id) values ('Thanks for the work', 1);
 insert into attachs(attachs_name, item_id) values('file', 1); 
