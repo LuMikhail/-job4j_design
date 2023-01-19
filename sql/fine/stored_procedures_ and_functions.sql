27ec8eb295b6e1e92c52f88119703872b8f03bff
Home <1984milu@mail.ru>
2023-01-19 11:44:31 +0300

    3. Хранимые процедуры и функции [#504804]

---
+++ sql/fine/stored_procedures_ and_functions.sql
@@ -0,0 +1,32 @@
+create or replace procedure delete_products(d_price integer)
+language 'plpgsql'
+as $$
+    BEGIN
+        if d_price > 0 THEN
+            delete from products 
+			where price > d_price;
+        end if;
+    END;
+$$;
+
+call delete_products(30);
+
+
+create or replace function delete_products_function(d_products_name varchar, d_count integer)
+returns void
+language 'plpgsql'
+as
+$$
+    begin
+        if products_name = d_products_name THEN
+             delete from products
+             where count = d_count;
+        end if;
+    end;
+$$;
+insert into products(products_name, producer, count, price) values ('product_3', 'producer_3', 8, 115);
+
+select delete_products_function('product_3', 8);
+
+
+
