76826cd2b143f777696a968b71cef04d8c5756a5
Home <1984milu@mail.ru>
2023-01-20 13:58:23 +0300

    9. Подзапросы [#504874]

--- sql/fine/create_fine.sql
+++ sql/fine/create_fine.sql
@@ -26,9 +26,3 @@ where sum_fine > 1400;
 
 select * from fines;
 
-INSERT INTO fines (names, number_plate, sum_fine, date_violation)
-VALUES ('Баранов П.Е.', 'Р523ВТ', 500, '2020-02-14 '),
-       ('Абрамова К.А.', 'О111АВ', 2000, '2020-02-23'),
-       ('Яковлев Г.Р.', 'Т330ТТ', 1500, '2020-03-03');
-
-select * from fines;
